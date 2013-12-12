74050e85f28dbf33f82b69290a7249f22fba5cf9
denevell <aaron@denevell.org>
2013-12-12 22:50:39 +0000

    Start of list posts controller and tests.

---
+++ src/org/denevell/droidnatch/posts/list/ListPostsController.java
@@ -0,0 +1,11 @@
+package org.denevell.droidnatch.posts.list;
+
+import org.denevell.droidnatch.app.interfaces.Controller;
+
+public class ListPostsController implements Controller {
+
+    @Override
+    public void go() {
+    }
+
+}
---
+++ src/org/denevell/droidnatch/posts/list/ListPostsControllerTests.java
@@ -0,0 +1,25 @@
+package org.denevell.droidnatch.posts.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListPostsControllerTests {
+
+    private ListPostsController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        controller = new ListPostsController();
+    }
+
+    @Test
+    public void shouldSetServiceCallback() throws Exception {
+        
+    }
+    
+    @Test
+    public void shouldCallServiceOnGoMethod() throws Exception {
+        
+    }
+        
+}
