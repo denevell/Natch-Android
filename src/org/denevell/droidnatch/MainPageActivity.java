b8128dbb9283e55d05038360e82ef8110cf489be
denevell <aaron@denevell.org>
2013-12-14 21:47:20 +0000

    Fixed rotate / fragment problem.
    Fixed a problem where the current fragments would be disgarded on
    rotate, since the main activity would just bug a new fragment regardless
    of whatever was previously there.

--- src/org/denevell/droidnatch/MainPageActivity.java
+++ src/org/denevell/droidnatch/MainPageActivity.java
@@ -5,6 +5,7 @@ import org.denevell.natch.android.R;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.Window;
 
@@ -18,11 +19,25 @@ public class MainPageActivity extends FragmentActivity {
         try {
             requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
             setContentView(R.layout.activity_main);
-            
-            getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.fragment_holder, new ListThreadsFragment())
-                .commit();
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to parse activity", e);
+            return;
+        }
+    }
+    
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        try {
+            FragmentManager supportFragmentManager = getSupportFragmentManager();
+            if(supportFragmentManager.getFragments()==null || 
+                    supportFragmentManager.getFragments().size()==0) {
+                Log.v(TAG, "Starting initial fragment");
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fragment_holder, new ListThreadsFragment())
+                    .commit();
+            }
         } catch (Exception e) {
             Log.e(TAG, "Failed to parse activity", e);
             return;
