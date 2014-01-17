61c96d8db0f092dcb8f5fe934a3179d8b38d590c
Denevell <aaron@denevell.org>
2014-01-17 13:32:20 +0000

    Update AppUtils.java

--- uiTests/org/denevell/droidnatch/uitests/utils/AppUtils.java
+++ uiTests/org/denevell/droidnatch/uitests/utils/AppUtils.java
@@ -15,16 +15,15 @@ public class AppUtils {
         allAppsButton.clickAndWaitForNewWindow();
         
         try {
-            UiObject appsTab = new UiObject(new UiSelector().text("OK"));
-            appsTab.click();
+            UiObject coachMarkOk = new UiObject(new UiSelector().text("OK"));
+            coachMarkOk.click();
         } catch(Exception e) {
             System.out.println("Couldn't press OK on the coach marks apps menu thing -- doesn't exist?");
         }
 
         UiObject demoWindow = new UiObject(new UiSelector().text("Apps"));
-        appsTab.click();        
+        demoWindow.click();        
 
-        
         UiScrollable appsView = new UiScrollable(new UiSelector().scrollable(true));
         appsView.setMaxSearchSwipes(20);
         appsView.setAsHorizontalList();
