ad1f13a985191db371c14ade3075f05bfc5d593f
denevell <aaron@denevell.org>
2014-01-26 21:57:53 +0000

    Changed to the real base path url.

--- src/org/denevell/droidnatch/ApplicationTests.java
+++ src/org/denevell/droidnatch/ApplicationTests.java
@@ -3,7 +3,6 @@ package org.denevell.droidnatch;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
