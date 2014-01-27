9be295b1e3a5faed790d1bfd1085bae620c3c779
denevell <aaron@denevell.org>
2014-01-27 10:29:31 +0000

    UiTests now point the app to local services.
    This is so the CI server can run them, since it runs its own version of the services.

--- uiTests/org/denevell/droidnatch/uitests/utils/NatchUiAutomatorTests.java
+++ uiTests/org/denevell/droidnatch/uitests/utils/NatchUiAutomatorTests.java
@@ -2,7 +2,6 @@ package org.denevell.droidnatch.uitests.utils;
 
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
-import org.denevell.droidnatch.Urls;
 import org.denevell.droidnatch.uitests.utils.AppUtils;
 import org.denevell.droidnatch.uitests.utils.UiConstants;
 
@@ -11,7 +10,6 @@ public class NatchUiAutomatorTests extends UiAutomatorTestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        Urls.setBasePath("http://192.168.43.103:8080/Natch-REST-ForAutomatedTests/rest/");
     }
 	
 }
