f36b76eb3f3f91d92bc5c0ddd319e608f93c957c
denevell <aaron@denevell.org>
2014-02-20 00:41:18 +0000

    Added ui test for register.

---
+++ uiTests/src/org/denevell/droidnatch/uitests/_000_Register.java
@@ -0,0 +1,34 @@
+package org.denevell.droidnatch.uitests;
+
+import java.util.Date;
+
+import org.denevell.droidnatch.MainPageActivity;
+import org.denevell.droidnatch.uitests.pageobjects.RegisterPO;
+import org.denevell.droidnatch.uitests.utils.NatchAndroidInstrumentation;
+
+public class _000_Register extends NatchAndroidInstrumentation {
+
+    public _000_Register() throws Exception {
+        super("org.denevell.natch.android", MainPageActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testSuccess() throws Exception {
+    	String username = "aaron"+new Date().getTime();
+		new RegisterPO()
+    		.register(getInstrumentation(), username, username)
+    		.registerSuccess();
+    }
+    
+    public void testFail() throws Exception {
+    	new RegisterPO()
+    		.register(getInstrumentation(), "aaron", "aaron") // Should be in the db from the base class
+    		.showRegisterError();
+    }
+
+
+}
---
+++ uiTests/src/org/denevell/droidnatch/uitests/pageobjects/RegisterPO.java
@@ -0,0 +1,57 @@
+package org.denevell.droidnatch.uitests.pageobjects;
+
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.clearText;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.pressImeActionButton;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.doesNotExist;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+
+import org.denevell.droidnatch.uitests.CustomMatchers;
+import org.denevell.natch.android.R;
+
+import android.app.Instrumentation;
+
+import com.google.android.apps.common.testing.ui.espresso.action.ViewActions;
+
+public class RegisterPO {
+
+	public RegisterPO register(Instrumentation instr, String username, String password) {
+		openActionBarOverflowOrOptionsMenu(instr.getTargetContext());
+        onView(withText("Register")).perform(click());
+        onView(withId(R.id.register_username_edittext))
+        	.perform(clearText(),
+        			typeText(username), 
+        			ViewActions.pressImeActionButton());
+        onView(withId(R.id.register_password_edittext))
+        	.perform(clearText(),
+        			typeText(password), 
+        			pressImeActionButton(),
+        			ViewActions.closeSoftKeyboard());
+        try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+        onView(withText("Register")).perform(click());
+        return this;
+	}
+
+	public RegisterPO showRegisterError() {
+        onView(withId(R.id.register_username_edittext))
+        	.check(matches(CustomMatchers.showsErrorString())); // Therefore fail
+        return this;
+	}
+
+	public RegisterPO registerSuccess() {
+        onView(withId(R.id.register_username_edittext))
+        	.check(doesNotExist()); // We thus know we've logged in okay
+        return this;
+	}
+
+
+}
