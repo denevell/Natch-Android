eca57afac277ef8e4cc0a993f03fddedc4ffcfc2
denevell <aaron@denevell.org>
2014-03-26 03:16:42 +0000

    Uat now goes to the secure url.
    Added ability to nuke ssl certificates

--- src/main/java/org/denevell/droidnatch/Application.java
+++ src/main/java/org/denevell/droidnatch/Application.java
@@ -1,5 +1,15 @@
 package org.denevell.droidnatch;
 
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import org.denevell.droidnatch.app.push.GcmServerRegister;
 
 import android.preference.PreferenceManager;
@@ -19,7 +29,10 @@ public class Application extends android.app.Application {
     @Override
     public void onCreate() {
         super.onCreate();
-	BugSenseHandler.initAndStartSession(Application.this, "c7e316a1");
+        if(getResources().getBoolean(R.bool.trust_all_certs)) {
+        	NukeSSLCerts.nuke();
+        }
+        BugSenseHandler.initAndStartSession(Application.this, "c7e316a1");
         appInstance = this;
         setBasePathIfEmpty();
 
@@ -47,4 +60,41 @@ public class Application extends android.app.Application {
         }
         return requestQueue;
     }
+    
+    public static class NukeSSLCerts {
+        protected static final String TAG = "NukeSSLCerts";
+     
+        public static void nuke() {
+            try {
+                TrustManager[] trustAllCerts = new TrustManager[] { 
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() {
+            	        /* Create a new array with room for an additional trusted certificate. */
+                            X509Certificate[] myTrustedAnchors = new X509Certificate[0];  
+                            return myTrustedAnchors;
+                        }
+            						
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+            		  
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                    }
+                };
+    			
+                SSLContext sc = SSLContext.getInstance("SSL");
+                sc.init(null, trustAllCerts, new SecureRandom());
+                HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+                HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                    @Override
+                    public boolean verify(String arg0, SSLSession arg1) {
+                        return true;
+                    }
+                });
+            } catch (Exception e) { 
+                // pass
+            }
+        }
+    }    
+    
 }
